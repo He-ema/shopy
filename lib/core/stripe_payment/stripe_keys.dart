ff5fd09b0c9db516a88363ef4c5302a3cec0656f
Ibrahim Osama <106915409+He-ema@users.noreply.github.com>
2023-10-07 17:18:26 +0300

    Update stripe_keys.dart

--- lib/core/stripe_payment/stripe_keys.dart
+++ lib/core/stripe_payment/stripe_keys.dart
@@ -1,6 +1,6 @@
 abstract class ApiKeys {
   static const String publishableKey =
-      'pk_test_51NuoOAApLcwrwsT2j2tOuTjy4vvxS6ZLEv8cKfeRAtFk1mqt0BTaFf1K8WRYcu4pXdo4I2ADYgGYHxgX0nmHQ0Io00hI0Ijvml';
+      'Hidden';
   static const String secretKey =
-      'sk_test_51NuoOAApLcwrwsT2zYLCmjHvd8sGhPaT1HXdvM3YC7EwSnFRUZb47i9ImpeOS4p1x3gcyaSWhtZbXT5BGWq5P1N000FHcSUzMl';
+      'Hidden';
 }
